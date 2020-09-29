41b3c161169454e87addfdaac17c8bbcd42fa832
utkarsh <avengers@marvel.com>
2020-09-29 11:54:23 +0000

    removed the arrows

--- MadLib.py
+++ MadLib.py
@@ -5,12 +5,12 @@
 loop = 1
 while (loop < 10):
 
-    noun = input("Choose a noun:-> ")
-    p_noun = input("Choose a plural noun:-> ")
-    noun2 = input("Choose a noun:-> ")
-    place = input("Name a place:-> ")
-    adjective = input("Choose an adjective (Describing word):-> ")
-    noun3 = input("Choose a noun:-> ")
+    noun = input("Choose a noun: ")
+    p_noun = input("Choose a plural noun:")
+    noun2 = input("Choose a noun: ")
+    place = input("Name a place:")
+    adjective = input("Choose an adjective (Describing word): ")
+    noun3 = input("Choose a noun: ")
 
     print ("------------------------------------------")
     print ("Be kind to your",noun,"- footed", p_noun)
